5db12ed9bf4530edc1bc774ac0e5e1381102f3bb
Ivan.Danylenko <Ivan.Danylenko@pl.ey.com>
2024-04-26 18:38:39 +0000

    Fixed error

--- docker/postgres/marketing_campaign.sql
+++ docker/postgres/marketing_campaign.sql
@@ -1,26 +1,26 @@
 CREATE SCHEMA marketing_campaign;
 
-CREATE TABLE marketing_campaign.campaigns (
-    contact_id SERIAL PRIMARY KEY,
-    client_id INTEGER REFERENCES marketing_campaign.clients(client_id) NOT NULL,
-    contact_date DATE NOT NULL,
-    contact_outcome VARCHAR(256) NOT NULL
-);
-
 CREATE TABLE marketing_campaign.clients (
     client_id SERIAL PRIMARY KEY,
-    age INTEGER NOT NULL,
-    job VARCHAR(50) NOT NULL,
-    marital VARCHAR(50) NOT NULL,
-    education VARCHAR(50) NOT NULL,
-    "default" BOOLEAN NOT NULL,
-    balance INTEGER NOT NULL,
-    housing BOOLEAN NOT NULL,
-    loan BOOLEAN NOT NULL,
-    contact VARCHAR(50) NOT NULL,
-    day INTEGER NOT NULL,
-    month VARCHAR(50) NOT NULL,
-    duration INTEGER NOT NULL,
-    campaign INTEGER NOT NULL,
-    y BOOLEAN NOT NULL
+    age INTEGER,
+    job VARCHAR(50),
+    marital VARCHAR(50),
+    education VARCHAR(50),
+    "default" BOOLEAN,
+    balance INTEGER,
+    housing BOOLEAN,
+    loan BOOLEAN,
+    contact VARCHAR(50),
+    day INTEGER,
+    month VARCHAR(50),
+    duration INTEGER,
+    campaign INTEGER,
+    y BOOLEAN
+);
+
+CREATE TABLE marketing_campaign.campaigns (
+    contact_id SERIAL PRIMARY KEY,
+    client_id INTEGER REFERENCES marketing_campaign.clients(client_id),
+    contact_date DATE,
+    contact_outcome VARCHAR(256)
 );
