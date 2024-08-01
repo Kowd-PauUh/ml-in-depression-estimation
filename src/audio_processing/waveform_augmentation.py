b2fc8c695e103c501bfa3a59dbae3ccde5ed0c76
Ivan.Danylenko <Ivan.Danylenko@pl.ey.com>
2024-08-01 16:30:57 +0000

    Implemented mixup augmentation, added waveform trimming to resampling augmentation function

--- src/audio_processing/waveform_augmentation.py
+++ src/audio_processing/waveform_augmentation.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import warnings
 import random
 
@@ -21,7 +22,11 @@ def normalize_by_std(waveform: torch.Tensor):
     return waveform / std
 
 
-def random_resample(waveform: torch.Tensor, orig_sample_rate: int) -> torch.Tensor:
+def random_resample(
+    waveform: torch.Tensor, 
+    orig_sample_rate: int, 
+    trim: bool = True
+) -> torch.Tensor:
     """
     Resamples the waveform by a random factor uniformly chosen in range [0.8, 1.25].
 
@@ -31,17 +36,23 @@ def random_resample(waveform: torch.Tensor, orig_sample_rate: int) -> torch.Tens
         The original waveform.
     orig_sample_rate : int
         The original sample rate of the waveform.
+    trim : bool, optional
+        Boolean flag on whether to trim the resampled waveform to its original length.
 
     Returns
     -------
     torch.Tensor
         The resampled waveform.
     """
+    original_len = waveform.size(1)
     resample_factor = random.uniform(0.8, 1.25)
     new_sample_rate = int(orig_sample_rate * resample_factor)
     
     resampler = T.Resample(orig_freq=orig_sample_rate, new_freq=new_sample_rate)
     resampled_waveform = resampler(waveform)
+
+    if trim:
+        resampled_waveform = resampled_waveform[:, :original_len]
     
     return resampled_waveform
 
@@ -53,3 +64,44 @@ def random_gain(waveform: torch.Tensor) -> torch.Tensor:
     
     waveform_with_gain = waveform * gain_linear
     return waveform_with_gain
+
+
+def mixup(
+    waveform1: torch.Tensor,
+    waveform2: torch.Tensor,
+    alpha: float = 0.2
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    """
+    Performs mixup on two waveforms.
+
+    Note
+    ----
+    The longer waveform will be trimmed to the length of shorter one.
+
+    Parameters
+    ----------
+    waveform1 : torch.Tensor
+        The first waveform tensor.
+    waveform2 : torch.Tensor
+        The second waveform tensor.
+    alpha : float, optional
+        The mixup interpolation parameter. Defaults to 0.2.
+
+    Returns
+    -------
+    Tuple[torch.Tensor, torch.Tensor]
+        Tuple containing two mixed waveforms.
+    """
+    # Ensure both waveforms have the same length
+    min_length = min(waveform1.size(1), waveform2.size(1))
+    waveform1 = waveform1[:, :min_length]
+    waveform2 = waveform2[:, :min_length]
+    
+    # Sample lambda from Beta distribution
+    lam = torch.distributions.Beta(alpha, alpha).sample()
+    
+    # Perform mixup
+    mixed_waveform1 = lam * waveform1 + (1 - lam) * waveform2
+    mixed_waveform2 = (1 - lam) * waveform1 + lam * waveform2
+    
+    return mixed_waveform1, mixed_waveform2
