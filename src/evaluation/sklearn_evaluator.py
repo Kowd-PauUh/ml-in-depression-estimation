4bc1615d78c6ea7c2e04a59bf2627b20b694c2cc
Ivan.Danylenko <Ivan.Danylenko@pl.ey.com>
2024-08-13 08:39:46 +0000

    Added preprocessing func. and data groups to SklearnEvaluator

--- src/evaluation/sklearn_evaluator.py
+++ src/evaluation/sklearn_evaluator.py
@@ -2,23 +2,27 @@ from typing import List, Callable
 
 import pandas as pd
 
-from src.evaluation.abs_evaluator import Evaluator
+from src.evaluation.base_evaluator import BaseEvaluator
 
 
-class SklearnEvaluator(Evaluator):
+class SklearnEvaluator(BaseEvaluator):
     def __init__(
         self, 
         model, 
+        preprocessing_fn: Callable,
         inference_fn: Callable,
         X: pd.DataFrame, 
         Y: pd.DataFrame, 
+        data_groups: pd.Series,
         score_fns: List[Callable]
     ):
         super().__init__()
 
         self.model = model
+        self.preprocessing_fn = preprocessing_fn
         self.inference_fn = inference_fn
         self.data = pd.concat([X.copy(), Y.copy()], axis=1)
+        self.data_groups = data_groups
         self.target = Y.columns[0]
 
         self.score_fns = score_fns
@@ -29,13 +33,19 @@ class SklearnEvaluator(Evaluator):
     def train_and_evaluate(self, train_index, test_index, **kwargs):
         metrics = {fn.__name__: [] for fn in self.score_fns}
 
+        X_train, X_test = self.preprocessing_fn(
+            data=self.data.drop(self.target, axis=1),
+            train_index=train_index,
+            test_index=test_index
+        )
+
         self.model.fit(
-            self.data.iloc[train_index].drop(self.target, axis=1), 
+            X_train, 
             self.data.iloc[train_index][self.target]
         )
 
         y_test = self.data.iloc[test_index][self.target].to_numpy()
-        y_pred = self.inference_fn(self.data.iloc[test_index].drop(self.target, axis=1))
+        y_pred = self.inference_fn(X_test)
 
         for fn in self.score_fns:
             metrics[fn.__name__].append(fn(y_test, y_pred, **kwargs))
