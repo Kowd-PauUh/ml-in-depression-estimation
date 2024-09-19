3356fba29a407bc10a47a5dc6d1ca59a0556af9a
Ivan.Danylenko <Ivan.Danylenko@pl.ey.com>
2024-09-19 20:18:47 +0000

    Updated logging

--- src/training/fine_tuning/training_module.py
+++ src/training/fine_tuning/training_module.py
@@ -120,7 +120,7 @@ class FineTuningTrainingModule(L.LightningModule):
             in_features = self.cnn.fc.in_features
             self.cnn.fc = nn.Linear(in_features, 1)
         else:
-            raise ValueError(f"Unsupported CNN architecture: {self.cnn.__class__}")
+            raise ValueError(f"Unsupported CNN architecture: {self.cnn.__class__.__name__}")
 
     def _validate_init(self):
         allowed_objectives = ['classification', 'regression']
@@ -145,7 +145,7 @@ class FineTuningTrainingModule(L.LightningModule):
             )
 
         logger.info(
-            f'Training {self.cnn.__class__} with {self.objective} objective '
+            f'Training {self.cnn.__class__.__name__} with {self.objective} objective '
             f'(mel_bins = {self.mel_bins}, augmentation = {self.augmentation}, '
             f'chunking_strategy = {self.chunking_strategy}, lr = {self.lr}, '
             f'lr_reduction_factor = {self.lr_reduction_factor}, lr_patience = {self.lr_patience}'
