25d0520cb9f93c3bc16832e05a461bc01dcbfc73
Ivan <ioann.danylenko@gmail.com>
2024-11-13 16:39:59 +0000

    Renamed losses

--- src/training/loss_monitor.py
+++ src/training/loss_monitor.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 import lightning as L
+import torch
 
 
 class LossMonitor(L.Callback):
@@ -13,29 +14,29 @@ class LossMonitor(L.Callback):
 
     def on_train_end(self, trainer, pl_module):
         log_dir = pl_module.logger.log_dir
-        df = pd.read_csv(f"{log_dir}/metrics.csv")
+        df = pd.read_csv(f'{log_dir}/metrics.csv')
 
         # plot train loss
         df['epoch'] += 1
-        _df = df[~df['train_loss'].isna()].copy()
+        _df = df[~df['train_loss_step'].isna()].copy()
         if not self.average_train_loss:
             _df['step'] = _df['step'] / trainer.num_training_batches
 
         sns.lineplot(
             data=_df, 
             x='epoch' if self.average_train_loss else 'step', 
-            y='train_loss', 
+            y='train_loss_step', 
             marker='o' if self.average_train_loss else None, 
-            label='train_loss'
+            label='train_loss_step'
         )
 
         # plot val loss
         sns.lineplot(
-            data=df[~df['val_loss'].isna()].copy(), 
+            data=df[~df['val_loss_step'].isna()].copy(), 
             x='epoch', 
-            y='val_loss', 
+            y='val_loss_step', 
             marker='o', 
-            label='val_loss'
+            label='val_loss_step'
         )
 
         # save plot
@@ -45,7 +46,7 @@ class LossMonitor(L.Callback):
         plt.savefig(
             f'{log_dir}/loss_curve.{self.save_format}', 
             format=self.save_format, 
-            bbox_inches="tight"
+            bbox_inches='tight'
         )
         plt.close()
 
