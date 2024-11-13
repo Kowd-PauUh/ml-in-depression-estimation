21ff0d037bf73e7d6c321278909a37ee3e16a056
Ivan <ioann.danylenko@gmail.com>
2024-11-13 18:17:25 +0000

    Implemented workaround for epoch logging issue

---
+++ src/training/epoch_logger.py
@@ -0,0 +1,40 @@
+"""
+Workaround for the `lightning` issue described in:
+https://github.com/Lightning-AI/pytorch-lightning/issues/3228
+
+Use `EpochLogger` as a callback for the `lightning.Trainer`
+to ensure the epoch (not global step) is displayed in MLFlow 
+for epoch-wise metrics.
+
+Notes
+-----
+The callback overrides the logged "step" and sets its value to 
+`lightning.Trainer.current_epoch`.
+
+Exapmles
+--------
+>>> import lightning as L
+>>> from src.training.epoch_logger import EpochLogger
+>>> epoch_logger = EpochLogger()
+>>> trainer = L.Trainer(
+...     callbacks=[epoch_logger],  # `EpockLogger` instance is a `lightning.Callback`
+... )
+"""
+import lightning as L
+
+
+class EpochLogger(L.Callback):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def on_train_epoch_end(self, trainer, pl_module):
+        self._log_epoch(trainer, pl_module)
+
+    def on_test_epoch_end(self, trainer, pl_module):
+        self._log_epoch(trainer, pl_module)
+
+    def on_validation_epoch_end(self, trainer, pl_module):
+        self._log_epoch(trainer, pl_module)
+
+    def _log_epoch(self, trainer, pl_module):
+        pl_module.log('step', trainer.current_epoch)
