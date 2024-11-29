64607f4e63f4faba223fef64081a8fd826be5a89
Ivan <ioann.danylenko@gmail.com>
2024-11-29 22:44:35 +0000

    Cross validation usage

--- src/scripts/fine_tuning/batch_size_tuning.sh
+++ src/scripts/fine_tuning/batch_size_tuning.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
+# training params
 model_name="resnet18"
 objective="regression"
-batch_size_space="16 32 48 64 80 96"
+batch_size_space="4 8 16 32 48 64"
 train_chunking_strategy="random"
 eval_chunking_strategy="truncate"
 ram_optimized_mode=False
 max_epochs=30
 
+# cross-validation params
+n_folds=5
+n_repetitions=2
+
 for batch_size in $batch_size_space; do
-    python3 $PROJECT_DIR/src/scripts/fine_tune_cnn.py \
+    python3 $PROJECT_DIR/src/scripts/repeated_fine_tune_cnn.py \
       --model_name=${model_name} \
       --objective=${objective} \
       --train_chunking_strategy=${train_chunking_strategy} \
       --eval_chunking_strategy=${eval_chunking_strategy} \
       --batch_size=${batch_size} \
       --ram_optimized_mode=${ram_optimized_mode} \
-      --max_epochs=${max_epochs}
+      --max_epochs=${max_epochs} \
+      --n_folds=${n_folds} \
+      --n_repetitions=${n_repetitions}
 done
