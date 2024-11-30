a78cb043ee21c8d14a7d970efd88276212da291f
Ivan <ioann.danylenko@gmail.com>
2024-11-30 09:09:28 +0000

    Specified ugmentation

--- src/scripts/fine_tuning/batch_size_tuning.sh
+++ src/scripts/fine_tuning/batch_size_tuning.sh
@@ -3,6 +3,7 @@
 # training params
 model_name="resnet18"
 objective="regression"
+augmentation="None"
 batch_size_space="4 8 16 32 48 64"
 train_chunking_strategy="random"
 eval_chunking_strategy="truncate"
@@ -22,6 +23,7 @@ for batch_size in $batch_size_space; do
       --batch_size=${batch_size} \
       --ram_optimized_mode=${ram_optimized_mode} \
       --max_epochs=${max_epochs} \
+      --augmentation=${augmentation} \
       --n_folds=${n_folds} \
       --n_repetitions=${n_repetitions}
 done
