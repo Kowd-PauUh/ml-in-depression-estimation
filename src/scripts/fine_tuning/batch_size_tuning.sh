1531120e072bfeb091ce6889587e6ec990dc13c7
Ivan <ioann.danylenko@gmail.com>
2024-12-11 08:16:48 +0000

    Set default training startegy to transformer

--- src/scripts/fine_tuning/batch_size_tuning.sh
+++ src/scripts/fine_tuning/batch_size_tuning.sh
@@ -5,8 +5,8 @@ model_name="resnet18"
 objective="regression"
 augmentation="None"
 batch_size_space="4 8 16 32 48 64"
-train_chunking_strategy="mean"
-eval_chunking_strategy="mean"
+train_chunking_strategy="transformer"
+eval_chunking_strategy="transformer"
 ram_optimized_mode=False
 max_epochs=30
 
