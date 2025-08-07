49c745d263f6973d0b737a6627efeda32326b327
Ivan <kowd.pauuh@gmail.com>
2025-08-07 13:22:51 +0000

    Added data_leakage argument

--- src/scripts/fine_tuning/reproduce_no_leakage.sh
+++ src/scripts/fine_tuning/reproduce_no_leakage.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+data_leakage=False
+
 # training params
 model_name="resnet18"
 objective="regression"
@@ -18,6 +20,7 @@ n_repetitions=5
 python3 $PROJECT_DIR/src/scripts/fine_tuning/repeated_fine_tune_cnn.py \
   --model_name=${model_name} \
   --objective=${objective} \
+  --data_leakage=${data_leakage} \
   --train_chunking_strategy=${train_chunking_strategy} \
   --eval_chunking_strategy=${eval_chunking_strategy} \
   --batch_size=${batch_size} \
