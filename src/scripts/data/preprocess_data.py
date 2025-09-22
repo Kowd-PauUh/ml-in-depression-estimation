f37dcfd60900e0b6e323fd44f6ab0428260162f8
Ivan <kowd.pauuh@gmail.com>
2025-09-22 14:33:42 +0000

    Enabled filtering out non DAIC-WOZ participants

--- src/scripts/data/preprocess_data.py
+++ src/scripts/data/preprocess_data.py
@@ -9,6 +9,24 @@ import matplotlib.pyplot as plt
 from loguru import logger
 
 
+EDAIC = False  # if False, only participants from DAIC-WOZ are preserved
+DAIC_WOZ_PIDS = [
+    302, 307, 331, 335, 346, 367, 377, 381, 382, 388, 389, 390, 395, 403,
+    404, 406, 413, 417, 418, 420, 422, 436, 439, 440, 451, 458, 472, 476,
+    477, 482, 483, 484, 489, 490, 492, 303, 304, 305, 310, 312, 313, 315,
+    316, 317, 318, 319, 320, 321, 322, 324, 325, 326, 327, 328, 330, 333,
+    336, 338, 339, 340, 341, 343, 344, 345, 347, 348, 350, 351, 352, 353,
+    355, 356, 357, 358, 360, 362, 363, 364, 366, 368, 369, 370, 371, 372,
+    374, 375, 376, 379, 380, 383, 385, 386, 391, 392, 393, 397, 400, 401,
+    402, 409, 412, 414, 415, 416, 419, 423, 425, 426, 427, 428, 429, 430,
+    433, 434, 437, 441, 443, 444, 445, 446, 447, 448, 449, 454, 455, 456,
+    457, 459, 463, 464, 468, 471, 473, 474, 475, 478, 479, 485, 486, 487,
+    488, 491, 300, 301, 306, 308, 309, 311, 314, 323, 329, 332, 334, 337,
+    349, 354, 359, 361, 365, 373, 378, 384, 387, 396, 399, 405, 407, 408,
+    410, 411, 421, 424, 431, 432, 435, 438, 442, 450, 452, 453, 461, 462,
+    465, 466, 467, 469, 470, 480, 481
+]
+
 # data source & destination specification
 PROJECT_DIR = Path(os.environ['PROJECT_DIR'])
 RAW_DATA_PATH = PROJECT_DIR / 'data/raw_data'
@@ -35,6 +53,10 @@ if __name__ == '__main__':
         labels.fillna(path.stem.split('_')[0], inplace=True)
     labels.columns = [c.lower() for c in labels.columns]
 
+    # filter out E-DAIC participants
+    if not EDAIC:
+        labels = labels[labels['participant_id'].isin(DAIC_WOZ_PIDS)]
+
     # fill missing values (there are two of them) in the 'PTSD Severity' column with its median depending on binary  PCL-C score
     labels['ptsd severity'][labels['ptsd severity'] == 'test'] = labels[labels['ptsd severity'] == 'test'].apply(
         lambda row: labels['ptsd severity'][(labels['ptsd severity'] != 'test') & (labels['pcl-c (ptsd)'] == row['pcl-c (ptsd)'])].median(),
@@ -55,6 +77,10 @@ if __name__ == '__main__':
         transcripts = pd.concat([transcripts, pd.read_csv(path)], axis=0)
         transcripts.fillna(int(path.stem.split('_')[0]), inplace=True)
     transcripts.columns = [c.lower() for c in transcripts.columns]
+
+    # filter out E-DAIC participants
+    if not EDAIC:
+        transcripts = transcripts[transcripts['id'].isin(DAIC_WOZ_PIDS)]
 
     # obtain duration and words number for each recording
     transcripts['words_cnt'] = transcripts['text'].apply(lambda text: len(text.strip().split()))
